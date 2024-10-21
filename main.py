ae58ceed4174fc4fe916d2aa38921f30146c7769
Abishek Prasad Subramanian <110110648+AbishekPrasad@users.noreply.github.com>
2024-10-21 19:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ wind_speed = "https://mosdac.gov.in/apienergy/wind?lat=16.088&lon=79.805"
 dataset = solarEnergy(solar_flux, wind_speed)
 fullSolarData ,todaySolarData = dataClean(dataset)
 
-fullSolarData.to_csv('full_solar_dataset.csv', index=False, header=False, mode='a')
-todaySolarData.to_csv('today_solar_dataset.csv', index=False, header=False, mode='a')
+fullSolarData.to_csv('all_data.csv', index=False, header=False, mode='a')
+todaySolarData.to_csv('solar_data.csv', index=False, header=False, mode='a')
 
 print('Uploaded data sucessully!')
